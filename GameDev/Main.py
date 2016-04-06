22f3c8540c7817270dde559955a4f31e6ff5e5e7
Chris Archibald <carchi8py@gmail.com>
2016-04-05 18:10:21 -0700

    Added Shapes and background colors

--- GameDev/Main.py
+++ GameDev/Main.py
@@ -2,12 +2,19 @@ import pygame
 import sys
 
 pygame.init()
-screen = pygame.display.set_mode((640, 360), 0, 32)
+maxX = 1920
+maxY = 1080
+screen = pygame.display.set_mode((maxX, maxY), 0, 32)
 
 clock = pygame.time.Clock()
 FPS = 60
 totalFrames = 0
 
+clr1 = (22, 122, 211)
+clr2 = (255, 44, 166)
+clr3 = (34, 55, 245)
+
+i = 0
 while True:
     #Processes
     for event in pygame.event.get():
@@ -17,8 +24,16 @@ while True:
             sys.exit()
     #Logic
     totalFrames += 1
-    
+    i += 1
+    if i > 255:
+        i %= 255
+
     #Draw
+    screen.fill((90,i,180))
+    pygame.draw.line(screen, clr1, (0, 0), (maxX, maxY), 5)
+    pygame.draw.rect(screen, clr2, (40,40,400,200))
+    pygame.draw.circle(screen, clr3, (1500, 700), 100, 5)
+
     pygame.display.flip()
 
     #Other
