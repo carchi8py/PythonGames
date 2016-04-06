9558860c4223a3ba0cac4f9598b779b0db07c3db
Chris Archibald <carchi8py@gmail.com>
2016-04-05 17:50:47 -0700

    Added a Clock to our game

--- GameDev/Main.py
+++ GameDev/Main.py
@@ -4,10 +4,22 @@ import sys
 pygame.init()
 screen = pygame.display.set_mode((640, 360), 0, 32)
 
+clock = pygame.time.Clock()
+FPS = 60
+totalFrames = 0
+
 while True:
+    #Processes
     for event in pygame.event.get():
         #Allows us to quit the game
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    #Logic
+    totalFrames += 1
+    
+    #Draw
     pygame.display.flip()
+
+    #Other
+    clock.tick(FPS)
